671f7ea9c3b141dde098ffb583f30cd2d3cd43ba
priyanka murmu <priyankamurmu769@gmail.com>
2024-01-03 18:56:53 +0530

    type conversion

---
+++ 06-type conversion/script.js
@@ -0,0 +1,33 @@
+let amount1 = '100';
+console.log(amount1,typeof amount1);
+
+// change string to number
+// amount = parseint(amount);
+// amount = +amount;
+// amount = number(amount);
+
+let amount2 = 100;
+console.log(amount2,typeof amount2);
+
+// change number to string
+// amount = amount.toString();
+// amount = string(amount);
+
+let amount3 = 99.5;
+console.log(amount3, typeof amount3);
+
+// change string to decimal
+// amount = parseFloat(amount);
+
+let amount4 = 1;
+console.log(amount4, typeof amount4);
+
+// change number to boolean
+ //amount = Boolean(amount);
+
+ let amount5 = 'hello';
+console.log(amount5, typeof amount5);
+console.log(Math.sqrt(-1));
+console.log(1 + NaN);
+console.log(undefined + undefined);
+console.log('foo'/ 3);
