6c4565bb85eabfccbed5f99e93f1f8bdf7aa8ab6
priyanka murmu <priyankamurmu769@gmail.com>
2024-01-06 19:30:20 +0530

    operators

---
+++ 07-Operators/script.js
@@ -0,0 +1,74 @@
+// arithmetic operation
+ 
+let x;
+
+x=5+5;
+y=5-5;
+z=3*5;
+a=15/3;
+b=5%5;
+
+console.log(x);
+console.log(z);
+
+// concatenation
+
+c="hello" + " world";
+console.log(c);
+
+// exponent
+x=2**5;
+console.log(x);
+
+// incriment
+x=1;
+// x=1+1;
+x++;
+
+console.log(x);
+
+// decrement
+// x=x-1;
+x--;
+console.log(x);
+
+// assignment
+x=10;
+
+x+=5;
+x=x+1;
+x-=5;
+x*=5;
+x/=5;
+x%=5;
+x**=5;
+
+console.log(x);
+
+// comparison operators
+x = 2 == '2';
+console.log(x);
+
+x = 2 === '2';
+console.log(x);
+
+x = 5=='3';
+console.log(x);
+
+x = 2 != '2';
+console.log(x);
+
+y = 3 != 5;
+console.log(y);
+
+x =15 > 5;
+console.log(x);
+
+y = 15 < 5;
+console.log(y);
+
+y = 12<=10;
+console.log(y);
+
+x =12 >= 10;
+console.log(x);
