51d67bf799b5b235100b6494e2d8a27b021d964a
Priya Murmu <priyankamurmu769@outlook.com>
2024-08-19 19:29:44 +0530

    while & do while loops

---
+++ 47- While & Do While Loops/script.js
@@ -0,0 +1,38 @@
+// let i = 0;
+
+// while (i <= 20) {
+  // console.log ('Number ' + i);
+  // i++;
+// }
+
+// Loop over Arrays
+// let i = 0;
+// const arr = [10, 20, 30, 40];
+
+// while (i < arr.length) {
+  // console.log(arr[i]);
+  // i++;
+// }
+
+// Reset i for the next loop
+
+// Nesting While Loops
+// i = 1;
+
+// while (i <= 5) {
+  // console.log('Number ' + i);
+
+  // let j = 1;
+  // while (j <= 5) {
+    // console.log(`${i} * ${j} = ${i * j}`);
+    // j++; // Increment j to avoid infinite loop
+  // }
+  // i++;
+// }
+
+i = 1;
+do {
+  console.log('Number ' + i);
+  i++;
+} while (i <= 20);
+
