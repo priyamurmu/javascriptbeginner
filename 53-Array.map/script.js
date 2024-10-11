ddf6cd125d2dd01bab91bc3690eb5cc32d807a86
Priya Murmu <priyankamurmu769@outlook.com>
2024-10-11 18:43:42 +0530

    Array.map

---
+++ 53-Array.map/script.js
@@ -0,0 +1,77 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const doubledNumbers = numbers.map((number) => number*2);
+
+console.log(doubledNumbers);
+
+// Same wih forEach
+
+const doubledNumbers2 = [];
+
+numbers.forEach((number) => {
+  doubledNumbers2.push(number*2);
+});
+console.log(doubledNumbers2);
+
+const companies = [
+  {name: 'Company One', category: 'Finance' ,start: 1981, end: 2004 },
+  { name: 'Company Two', category: 'Retail', start: 1992, end: 2008 },
+  {name: 'Company Three', category: 'Auto' , start: 1999, end: 2007 },
+  {name: 'Company Four', category: 'Retail' ,start: 1989, end: 2010 },
+  {name: 'Company Five', category: 'Technology' ,start: 2009, end: 2014 },
+  {name: 'Company Six', category: 'Finance' ,start: 1987, end: 2010 },
+  {name: 'Company Seven', category: 'Auto' ,start: 1986, end: 1996 },
+  {name: 'Company Eight', category: 'Technology' ,start: 2011, end: 2016 },
+  {name: 'Company Nine', category: 'Retail' ,start: 1981, end: 1989 },
+  
+]
+
+// Create an array of company name
+const companyNames = companies.map((company) => company.name);
+console.log(companyNames);
+
+// CREATE AN ARRAY with just company and category
+const companyInfo = companies.map((company) => {
+  return{
+    name: company.name,
+    category: company.category,
+  };
+});
+
+console.log(companyInfo);
+
+// Create an array of object with the length of each company in years
+
+const companyYears = companies.map((company) => {
+  return {
+    name: company.name,
+    length: company.end- company.start + 'years',
+  };
+});
+
+console.log(companyYears);
+
+// chain map methods
+const squareAndDouble = numbers.map((number) => Math.sqrt(number))
+.map((sqrt) => sqrt * 2);
+
+console.log(squareAndDouble);
+
+const squareAndDouble1 = numbers.map(function (number) {
+  return Math.sqrt(number); 
+  })
+.map(function (sqrt) {
+  return sqrt *2;
+})
+.map(function (sqrtDoubled) {
+  return sqrtDoubled * 3
+});
+
+
+console.log(squareAndDouble1);
+
+// chaining different method
+const evenDouble = numbers.filter((number) => number % 2 ===0)
+.map((number) => number * 2);
+
+console.log(evenDouble);
